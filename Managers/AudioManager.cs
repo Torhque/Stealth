7f10f262d0b1f7d393b68c4f6cf78e8c0394452a
Torhque <Torhque@live.com>
2017-02-19 19:25:49 -0700

    Add files via upload

---
+++ Managers/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	public static AudioManager manager;
+
+	// Music clips
+	public AudioClip menuSong;
+	public AudioClip levelSong;
+	public AudioClip deathSong;
+	public AudioClip winSong;
+	public AudioClip failSong;
+	public AudioSource musicPlayer; // AudioSource object for storing and swapping songs
+
+	// Pickup SFX
+	public AudioClip plainKeySound;
+	public AudioClip keyCardSound;
+	public AudioClip keyPadSound;
+	public AudioClip secretFilesSound;
+
+	// Door SFX
+	public AudioClip openMetalDoorSound;
+	public AudioClip lockedMetalDoorSound;
+	public AudioClip openWoodenDoorSound;
+	public AudioClip lockedWoodenDoorSound;
+	public AudioClip openKeypadDoorSound;
+	public AudioClip lockedKeypadDoorSound;
+
+	void Awake () {
+		if (manager == null) {
+			manager = this;
+
+			// Don't destroy this AudioManager!
+			DontDestroyOnLoad (gameObject);
+		} else {
+			Destroy (gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		musicPlayer = GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Managers/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+	public bool wasSpotted = false;
+
+	void Awake () {
+	}
+
+	// Use this for initialization
+	void Start () {
+		Screen.lockCursor = false;	
+
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad (this.gameObject);
+
+			AudioManager.manager.musicPlayer.clip = AudioManager.manager.menuSong; // swap the songs
+			AudioManager.manager.musicPlayer.Play (); // play the song
+
+		} else {
+			Destroy (this.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
